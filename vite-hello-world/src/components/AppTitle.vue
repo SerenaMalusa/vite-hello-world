d25faeee3115924f9ef8888e7baa8c5c14967b38
SerenaMalusa <sere.malu@gmail.com>
2024-02-07 17:36:23 +0100

    feat: add AppTitle component

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        data() {
+            return {
+                title: 'La mia prima app con Vite!'
+            }
+        },
+    }
+</script>
+
+<template>
+
+    <h1 class="title">{{ title }}</h1>
+
+</template>
+
+<style scoped>
+    .title {
+        margin: 50px auto;
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    
+</style>
